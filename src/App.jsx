ef57cab35829abd06183549b1328d8f637e01936
AvAValera <avavarenik07@gmail.com>
2022-02-10 14:44:43 +0200

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ export default function App() {
     return (
         <div className="app">
             <h1>HELLO WEBPACK TO REACT</h1>
+            <img className='logo' src="img/main/webpack.png" alt="webpack" />
         </div>
     )
 }
